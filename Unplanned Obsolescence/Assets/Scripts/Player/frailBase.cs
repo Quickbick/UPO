67ae97fe42a3334f0bc2d134db4e2d642910ad0a
Quickbick <nsostheller@gmail.com>
2023-06-13 15:32:02 -0700

    added grounded tag to disable multiple jumps

--- Unplanned Obsolescence/Assets/Scripts/Player/frailBase.cs
+++ Unplanned Obsolescence/Assets/Scripts/Player/frailBase.cs
@@ -6,6 +6,7 @@ public class frailBase : MonoBehaviour
 {
     public Rigidbody2D pcRigidBody;
     public Animator Animator;
+    bool grounded = true;
     float speed = 10f;
     float jumpAmount = 10f;
     int direction = 1;
@@ -23,8 +24,9 @@ public class frailBase : MonoBehaviour
         Vector3 playerInput;
 
         //Applies upwards force when jump is pressed
-        if (Input.GetKeyDown(KeyCode.Space)){
+        if (Input.GetKeyDown(KeyCode.Space) && grounded == true){
             pcRigidBody.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+            grounded = false;
         }
 
         //store player input as movement vector
@@ -32,7 +34,6 @@ public class frailBase : MonoBehaviour
         
         // Moves the character according to existing vectors
         pcRigidBody.AddForce(playerInput * speed);
-        //pcRigidBody.MovePosition(transform.position + playerInput * Time.deltaTime * speed);
         Animator.SetFloat("Movement_Horizontal", System.Math.Abs(playerInput.x));
         // Adjusts Direction of Model
         if (direction == 1 && playerInput.x < 0){
@@ -45,5 +46,10 @@ public class frailBase : MonoBehaviour
         }
 
         
+    }
+
+    //for collision with ground
+    void OnCollisionEnter2D(Collision2D col){
+        grounded = true;
     }
 }
