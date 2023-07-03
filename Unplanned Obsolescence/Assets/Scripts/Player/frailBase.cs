1cd3f3eb5e00f95265bc5670a4813dfb117d4a3b
Quickbick <nsostheller@gmail.com>
2023-07-03 15:56:02 -0700

    added standing attack and created lockout for during animations

--- Unplanned Obsolescence/Assets/Scripts/Player/frailBase.cs
+++ Unplanned Obsolescence/Assets/Scripts/Player/frailBase.cs
@@ -7,6 +7,7 @@ public class frailBase : MonoBehaviour
     public Rigidbody2D pcRigidBody;
     public Animator Animator;
     bool grounded = true;
+    bool lockout = false;
     float speed = 10f;
     float jumpAmount = 10f;
     int direction = 1;
@@ -24,42 +25,57 @@ public class frailBase : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 playerInput;
+        //if performing an action that locks out of others
+        if (lockout){
+            //locks out until current animation is finished
+            AnimatorStateInfo AnimInfo = Animator.GetCurrentAnimatorStateInfo(0);
+            if(AnimInfo.normalizedTime >= 1){
+                lockout = false;
+            }
+        }
+        else{
 
-        //Applies upwards force when jump is pressed
-        if (Input.GetKeyDown(KeyCode.Space) && grounded == true){
-            pcRigidBody.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
-            grounded = false;
-            Animator.SetTrigger("Jump");
-        }
+            Vector3 playerInput;
 
-        //checks for falling
-        float currentY = transform.position.y;
-        if (currentY + 0.01 < lastY){
-            Animator.SetTrigger("Fall");
-        }
-        if (currentY + 0.05 < lastY){
-            Animator.SetTrigger("Fall_Big");
-        }
-        lastY = currentY;
-        
-        //store player input as movement vector
-        playerInput = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-        
-        // Moves the character according to existing vectors
-        pcRigidBody.AddForce(playerInput * speed);
-        Animator.SetFloat("Movement_Horizontal", System.Math.Abs(playerInput.x));
-        // Adjusts Direction of Model
-        if (direction == 1 && playerInput.x < 0){
-            gameObject.transform.Rotate(new Vector3(0,180,0));
-            direction = 0;
-        }
-        else if (direction == 0 && playerInput.x > 0){
-            gameObject.transform.Rotate(new Vector3(0,180,0));
-            direction = 1;
-        }
+            //Applies upwards force when jump is pressed
+            if (Input.GetKeyDown(KeyCode.Space) && grounded == true){
+                pcRigidBody.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+                grounded = false;
+                Animator.SetTrigger("Jump");
+            }
 
+            if (Input.GetKeyDown(KeyCode.Z)){
+                Animator.SetTrigger("Attack_Start");
+                lockout = true;
+            }
+
+            //checks for falling
+            float currentY = transform.position.y;
+            if (currentY + 0.01 < lastY){
+                Animator.SetTrigger("Fall");
+            }
+            if (currentY + 0.05 < lastY){
+                Animator.SetTrigger("Fall_Big");
+            }
+            lastY = currentY;
         
+            //store player input as movement vector
+            playerInput = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        
+            // Moves the character according to existing vectors
+            pcRigidBody.AddForce(playerInput * speed);
+            Animator.SetFloat("Movement_Horizontal", System.Math.Abs(playerInput.x));
+            // Adjusts Direction of Model
+            if (direction == 1 && playerInput.x < 0){
+                gameObject.transform.Rotate(new Vector3(0,180,0));
+                direction = 0;
+            }
+            else if (direction == 0 && playerInput.x > 0){
+                gameObject.transform.Rotate(new Vector3(0,180,0));
+                direction = 1;
+            }
+
+        }
     }
 
     //for collision with ground
